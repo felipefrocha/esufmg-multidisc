f35d0256834aa3aa9f27af03d35935d3b345c44e
Renata Nésio <renata.nesio@mail.ru>
2021-08-23 08:45:24 -0300

    pep8 fixes

--- __init__.py
+++ __init__.py
@@ -11,16 +11,15 @@ log.setLevel(logging.DEBUG)
 FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=FORMAT)
 
-
 ###
 # END - Configure logs
 ###
-    
+
 if __name__ == "__main__":
     log.info('Initializing routines')
 
     for i in range(9):
-        cidades_raiz(i+1)
-    
+        cidades_raiz(i + 1)
+
     consolidar_cidades()
     exit(0)
