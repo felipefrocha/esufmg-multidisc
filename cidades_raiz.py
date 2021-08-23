c95c64d119678a0c292bee6ce35480ef28e11b17
Renata Nésio <renata.nesio@mail.ru>
2021-08-23 08:45:09 -0300

    Analysis: add temperature data to df

--- cidades_raiz.py
+++ cidades_raiz.py
@@ -5,72 +5,87 @@ Created on Sat Aug 14 20:11:15 2021
 @author: tiago
 """
 import logging
-import pandas as pd
-import numpy as np 
-from matplotlib import pyplot as plt
-import pyproj
-pyproj.Proj("+init=epsg:4326")
 
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
+import pyproj
+
+pyproj.Proj("+init=epsg:4326")
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=FORMAT)
+
+
 ###
 # END - Configure logs
 ###
 
-def cidades_raiz(numero_arquivo:int):
-
+def cidades_raiz(numero_arquivo: int):
     numero_arquivos = 2
-    
-    df = pd.read_csv(f'cidades_info/city_info{numero_arquivo}.csv') 
+
+    df = pd.read_csv(f'cidades_info/city_info{numero_arquivo}.csv')
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-    df = df.groupby(['Name',"ID"]).mean().reset_index()
-    
-    
-    
-    #kings_county_map.to_crs(epsg=4326).plot()
+    df = df.groupby(['Name', "ID"]).mean().reset_index()
+
+    # kings_county_map.to_crs(epsg=4326).plot()
     df["temp_max_fim"] = df['Lon']
     df["temp_max_inicio"] = df['Lon']
     df["temp_min_fim"] = df['Lon']
     df["temp_min_inicio"] = df['Lon']
+    df["ano_mais_quente"] = df['Lon']
+    df['temp_mais_alta'] = df['Lon']
+    df["ano_mais_frio"] = df['Lon']
+    df['temp_mais_baixa'] = df['Lon']
+
     for index, row in df.iterrows():
-    
         name = row["Name"]
         id_cidade = row["ID"]
-        df_cidade = pd.read_csv(f'cidades_raiz/{id_cidade}.csv') 
-        
+        df_cidade = pd.read_csv(f'cidades_raiz/{id_cidade}.csv')
+
         df_cidade = df_cidade.loc[:, ~df_cidade.columns.str.contains('^Unnamed')]
         df_cidade = df_cidade.drop(columns=['prcp'])
-        
+
         df_cidade['Date'] = df_cidade['Date'].str[0:4]
+        max_index = df_cidade['tmax'].idxmax()
+        df.loc[index, 'ano_mais_quente'] = df_cidade['Date'][max_index]
+        df.loc[index, 'temp_mais_alta'] = df_cidade['tmax'][max_index]
+
+        # df['ano_mais_quente'][index] = df_cidade['Date'][max_index]
+        # df['temp_mais_alta'][index] = df_cidade['tmax'][max_index]
+        min_index = df_cidade['tmin'].idxmin()
+        df.loc[index, 'ano_mais_frio'] = df_cidade['Date'][min_index]
+        df.loc[index, 'temp_mais_baixa'] = df_cidade['tmin'][min_index]
+
+        # df['ano_mais_frio'][index] = df_cidade['Date'][min_index]
+        # df['temp_mais_baixa'][index] = df_cidade['tmin'][min_index]
+
         df_cidade_mean = df_cidade.groupby(['Date']).mean()
-        
+
         date = df_cidade_mean.index
-        temp_max =  df_cidade_mean['tmax']
-        
+        temp_max = df_cidade_mean['tmax']
+
         temp_max_inicio = temp_max[30:].mean()
         temp_max_fim = temp_max[-30:].mean()
-        
-        temp_min =  df_cidade_mean['tmin']
+
+        temp_min = df_cidade_mean['tmin']
         temp_min_inicio = temp_min[30:].mean()
         temp_min_fim = temp_min[-30:].mean()
-        
-        fig = plt.figure(index+numero_arquivo*100)
-        plt.plot(date,temp_max,temp_min) 
+
+        fig = plt.figure(index + numero_arquivo * 100)
+        plt.plot(date, temp_max, temp_min)
         plt.savefig(f'saidas/cidades_temp_imagens/{name}.png')
         plt.close(fig)
-        
-        resultado = temp_max_fim-temp_max_inicio
-        df.loc[index,"temp_max_fim"]=temp_max_fim
-        df.loc[index,"temp_max_dif"]= resultado if resultado<2 else 2
-        
-    
-    df.to_csv(f'saidas/cidades_temp_result/out{numero_arquivo}.csv', index=False)  
-    
-    
 
+        resultado = temp_max_fim - temp_max_inicio
+        df.loc[index, "temp_max_fim"] = temp_max_fim
+        df.loc[index, "temp_max_dif"] = resultado if resultado < 2 else 2
 
+    df.to_csv(f'saidas/cidades_temp_result/out{numero_arquivo}.csv', index=False)
+
+    df.sort_values(by=['Lat'], inplace=True)
+    df.to_csv(f'saidas/cidades_temp_result/out_per_lat{numero_arquivo}.csv', index=False)
